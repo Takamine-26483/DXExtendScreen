5def10199a528d12e93374ecc8d8b61b75d39b2a
Takamine-26483 <twice.cake.1379@gmail.com>
2021-10-01 08:48:16 +0900

    プロジェクト ファイルを追加します。

---
+++ DXExtendScreen/DXExtendScreen.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using DxLibDLL;
+
+namespace TakamineProduction
+{
+	/// <summary>任意の倍率に拡大表示可能な描画可能グラフィック</summary>
+	public class DXExtendScreen
+	{
+		private double extendRate = 0;
+
+		/// <summary>描画可能グラフィックのハンドル</summary>
+		public int Handle { get; private set; }
+		/// <summary>描画可能グラフィックのドットバイドットでのサイズ</summary>
+		public Size ScreenOriginalSize { get; private set; }
+		/// <summary>描画可能グラフィックを実際に描画する際のサイズ</summary>
+		public Size ScreenSize { get; private set; }
+		/// <summary>拡大倍率（変更するとRemakeが実行される）</summary>
+		public double ExtendRate
+		{
+			get => extendRate;
+			set
+			{
+				extendRate = value;
+				Remake();
+			}
+		}
+		/// <summary>コンストラクタ。描画可能グラフィックを作成する</summary>
+		/// <param name="extendRate">拡大倍率</param>
+		public DXExtendScreen(double extendRate)
+		=> ExtendRate = extendRate;
+		/// <summary>現在のクライアント領域に合わせて各プロパティを再設定する。ハンドルは再作成される（SetDrawScreenでこのオブジェクトのハンドルを指定していた場合、再設定する）</summary>
+		public void Remake()
+		{
+			var ThisScreenIsTargeting = DX.GetDrawScreen() == Handle;
+
+			DX.GetScreenState(out int w, out int h, out _);
+			ScreenSize = new Size(w, h);
+			ScreenOriginalSize = new Size((int)(w / ExtendRate), (int)(h / ExtendRate));
+
+			DX.DeleteGraph(Handle);
+			Handle = DX.MakeScreen(ScreenOriginalSize.Width, ScreenOriginalSize.Height, 1);
+
+			if (ThisScreenIsTargeting)
+				DX.SetDrawScreen(Handle);
+		}
+		/// <summary>SetDrawScreenを実行する</summary>
+		/// <returns>SetDrawScreenの戻り値</returns>
+		public int SetAsDrawTarget() => DX.SetDrawScreen(Handle);
+		/// <summary>裏画面に描画可能グラフィックを描き込んでScreenFlipを実行する（SetDrawScreenはこのメソッド実行前に戻される）</summary>
+		public void DrawToBackAndFlip()
+		{
+			var buf = DX.GetDrawScreen();
+			DX.SetDrawScreen(DX.DX_SCREEN_BACK);
+
+			DX.DrawExtendGraph(0, 0, ScreenSize.Width, ScreenSize.Height, Handle, 0);
+			DX.ScreenFlip();
+
+			DX.SetDrawScreen(buf);
+		}
+	}
+}
---
+++ Sample/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using TakamineProduction;
+using DxLibDLL;
+
+namespace Sample
+{
+	static class Program
+	{
+		/// <summary>
+		/// アプリケーションのメイン エントリ ポイントです。
+		/// </summary>
+		[STAThread]
+		static void Main()
+		{
+			DX.ChangeWindowMode(1);
+			DX.SetGraphMode(800, 600, 32);
+			DX.DxLib_Init();
+			
+			var i = 0;
+			int pic = DX.LoadGraph("pic.jpg");
+			
+			//★　インスタンスを作成（１倍で作成）
+			var exScr = new DXExtendScreen(1);
+
+			//★　SetDrawScreenを実行
+			exScr.SetAsDrawTarget();
+
+
+			while(DX.ProcessMessage() != -1)
+			{
+				DX.ClearDrawScreen();
+
+
+				if (++i % 60 == 0)//★　１秒ごとに倍率を１倍増やす
+					exScr.ExtendRate += 1;
+				
+				DX.DrawGraph(0, 0, pic, 0);
+				DX.DrawPixel(100, 20, DX.GetColor(255, 0, 0));
+				DX.DrawLine(80, 30, 120, 30, DX.GetColor(255, 0, 0));
+				DX.DrawString(0, 0, exScr.ExtendRate.ToString("F2") + "倍　で表示。", DX.GetColor(255, 0, 255));
+
+				//★　裏画面に描画可能グラフィックを描き込んでScreenFlipを実行する
+				exScr.DrawToBackAndFlip();
+			}
+
+
+			DX.DxLib_End();
+		}
+	}
+}
